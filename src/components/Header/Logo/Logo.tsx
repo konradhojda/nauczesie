ab982b354eb8e1f9e0115e6372a9471fdc32d81d
Konrad Hojda <konrad.hojda@orange.com>
2023-05-26 22:55:57 +0200

    fix: fix logo url path

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Image
-      src="/dog-silhouette-logo.png"
+      src="./dog-silhouette-logo.png"
       alt="kitten"
       className="max-h-[60px]"
       width={60}
